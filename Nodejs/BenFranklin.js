7bc14913b1c65980e58fa667034a6c72ee427f36
sawantv02 <vishakhasawa@gmail.com>
2017-09-07 12:26:13 -0400

    js functions

---
+++ Nodejs/BenFranklin.js
@@ -0,0 +1,16 @@
+var EventEmitter=require('events').EventEmitter;
+var util=require('util');
+
+var Person=function(name){
+	this.name=name;
+};
+
+util.inherits(Person,EventEmitter);
+
+var ben=new Person("Ben Franklin");
+
+ben.on('speak',function(said){
+	console.log(`${this.name}:${said}`);
+});
+
+ben.emit('speak',"you may delay.");

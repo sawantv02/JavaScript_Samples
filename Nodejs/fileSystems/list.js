4e8a1679647db1c096966d2365412a4781f70492
sawantv02 <vishakhasawa@gmail.com>
2017-09-07 14:53:30 -0400

    file system module in node js

---
+++ Nodejs/fileSystems/list.js
@@ -0,0 +1,11 @@
+var fs=require('fs');
+
+fs.readdir('./lib',function(err,files){
+	if(err){
+		throw err;
+	}
+	
+	console.log(files);
+});
+
+console.log("Reading files....");
---
+++ Nodejs/fileSystems/read.js
@@ -0,0 +1,15 @@
+var fs=require('fs');
+var path=require('path');
+
+fs.readdir("./lib",function(err,files){
+	files.forEach(function(fileName){
+		var file=path.join(__dirname,"lib",fileName);
+		var stats=fs.statSync(file);
+		
+		if(stats.isFile()){
+			fs.readFile(file,"UTF-8",function(err,contents){
+				console.log(contents);
+			});
+		}
+	});
+});

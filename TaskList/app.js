d6436764ef798452fdcb13d87f26e25c82654f22
Vishakha Sawant <vsawant@zscaler.com>
2018-11-09 14:37:51 -0800

    tasklist

---
+++ TaskList/app.js
@@ -0,0 +1,162 @@
+//Define UI variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+//load all event listeners
+loadEventListeners();
+
+//load all event listeners
+function loadEventListeners() {
+	//DOM load event
+	document.addEventListener('DOMContentLoaded',getTasks);
+	//Add task event
+	form.addEventListener('submit',addTask);
+	//remove tasks event
+	taskList.addEventListener('click',removeTask);
+	//clear task event
+	clearBtn.addEventListener('click',clearTasks);
+	//filter task
+	filter.addEventListener('keyup',filterTasks);
+}
+//get tasks from LS
+function getTasks() {
+	let tasks;
+	if(localStorage.getItem('tasks') === null) {
+		tasks = [];
+	}else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+
+	tasks.forEach(function(task){
+		//create li element
+		const li = document.createElement('li');
+		//add class
+		li.className = 'collection-item';
+		//create text node and append to li
+		li.appendChild(document.createTextNode(task));
+		//create new link element
+		const link = document.createElement('a');
+		//add class
+		link.className = 'delete-item secondary-content';
+		//add icon html
+		link.innerHTML = '<i class="fa fa-remove"></i>';
+		//append the link to li
+		li.appendChild(link);
+		//append li to ul
+		taskList.appendChild(li);
+	});
+}
+
+//Add task
+function addTask(e) {
+	if(taskInput.value === ''){
+		alert("Add a task");
+	}else {
+	//create li element
+	const li = document.createElement('li');
+	//add class
+	li.className = 'collection-item';
+	//create text node and append to li
+	li.appendChild(document.createTextNode(taskInput.value));
+	//create new link element
+	const link = document.createElement('a');
+	//add class
+	link.className = 'delete-item secondary-content';
+	//add icon html
+	link.innerHTML = '<i class="fa fa-remove"></i>';
+	//append the link to li
+	li.appendChild(link);
+	//append li to ul
+	taskList.appendChild(li);
+
+	//store in localstorage
+	storeTaskInLocalStorage(taskInput.value);
+	//clear input
+	taskInput.value='';
+	e.preventDefault();
+	}
+}
+
+function storeTaskInLocalStorage(task) {
+	let tasks;
+	if(localStorage.getItem('tasks') === null) {
+		tasks = [];
+	}else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+
+	tasks.push(task);
+	localStorage.setItem('tasks',JSON.stringify(tasks));
+
+}
+
+//remove task
+function removeTask(e) {
+	if(e.target.parentElement.classList.contains('delete-item')) {
+		if(confirm('Are you sure?')){
+			e.target.parentElement.parentElement.remove();
+
+			//remove from LS
+			removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+		}
+
+
+	}
+}
+
+//remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+	let tasks;
+	if(localStorage.getItem('tasks') === null) {
+		tasks = [];
+	}else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+
+	tasks.forEach(function(task,index){
+		if(taskItem.textContent === task){
+			tasks.splice(index,1);
+		}
+	});
+
+	localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+
+//clear tasks
+function clearTasks() {
+	//taskList.innerHTML = '';
+
+	//faster
+	while(taskList.firstChild) {
+		taskList.removeChild(taskList.firstChild);
+	}
+
+	clearTasksFromLocalStorage();
+}
+
+//clear from LS
+function clearTasksFromLocalStorage() {
+	localStorage.clear();
+}
+
+//filter tasks
+function filterTasks(e) {
+	const text = e.target.value.toLowerCase();
+
+	document.querySelectorAll('.collection-item').forEach(function(task){
+		const item = task.firstChild.textContent.toLowerCase();
+		if(item.indexOf(text) != -1){
+			task.style.display = 'block';
+		} else {
+			task.style.display = 'none';
+		}
+	});
+
+
+
+}
+
+

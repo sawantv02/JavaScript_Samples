6ef504434dda4c743b49e672c7dc83ada7b0b819
Vishakha Sawant <vsawant@zscaler.com>
2018-11-11 19:27:06 -0800

    easyHttp using async-await

---
+++ EasyHTTP3/easyhttp3.js
@@ -0,0 +1,52 @@
+class EasyHTTP {
+	//get
+	async get(url) {
+		const response = await fetch(url);
+		const resData = await response.json();
+		return resData;
+	}
+
+	//post
+	async post(url,data) {
+
+		const response = await fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-type':'application/json'
+				},
+				body: JSON.stringify(data)
+			});
+		const resData = await response.json();
+		return resData;
+	
+	}
+
+	//put
+	async put(url,data) {
+
+		const response = await fetch(url, {
+				method: 'PUT',
+				headers: {
+					'Content-type':'application/json'
+				},
+				body: JSON.stringify(data)
+			});
+		const resData = await response.json();
+		return resData;
+	
+	}
+
+	//delete
+	async delete(url) {
+
+		const response = await	fetch(url, {
+				method: 'DELETE',
+				headers: {
+					'Content-type':'application/json'
+				}
+			});
+		const resData = await 'Resource Deletet...';
+		return resData;
+	
+	}
+}
